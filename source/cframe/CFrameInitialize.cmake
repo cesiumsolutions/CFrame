4af9ca85bed022f03d54f5d2e20aaa88fa56699c
csklu <csklu@cesiumsolutions.com>
2022-10-05 13:19:21 +0200

    CFrame: Move and remove files to modules directory. Update initialize

--- source/cframe/CFrameInitialize.cmake
+++ source/cframe/CFrameInitialize.cmake
@@ -12,17 +12,6 @@ include( CFrameMessage )
 include( CFrameListUtilities )
 include( CFrameDirectoryUtilities )
 
-# Configuration options
-include( CFrameGeneralConfiguration )
-
-#include_directories( testtools )
-
-#include( CFrameInternal )
-#include( CFrameUtilities )
-#include( CFrameExternalPackages )
-#include( CFrameProjects )
-
-
 # High-level CFrame-specific Utilities
 include( CFrameModuleTraversal )
 cframe_load_modules()
