ef9d551f3f447671472da30a7e79e3045112d564
csklu <csklu@cesiumsolutions.com>
2022-10-16 12:59:51 +0200

    CFrame/Modules: LibraryLoader.cmake added.

---
+++ share/cframe/modules/CFrameLibraryLoader.cmake
@@ -0,0 +1,24 @@
+# -----------------------------------------------------------------------------
+#
+# Initializes various variables related to platform-specific library loading
+#
+# -----------------------------------------------------------------------------
+
+# ----------------------------------------
+# Handle selection of C++ standard Version
+# ----------------------------------------
+
+if ( ("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux") OR
+     ("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin") )
+  set(
+      CFRAME_SYSTEM_LOADER_LIBRARIES dl
+      CACHE STRING
+      "Libraries to link for runtime dynamic loading."
+  )
+else()
+  set(
+      CFRAME_SYSTEM_LOADER_LIBRARIES
+      CACHE STRING
+      "Libraries to link for runtime dynamic loading."
+  )
+endif()
