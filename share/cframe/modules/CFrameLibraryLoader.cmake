5322b69489874ffa3c7588f7078ff38ad7234ac0
csklu <csklu@cesiumsolutions.com>
2022-12-15 11:32:52 +0100

    CFrame/Modules: SYSTEM_LOADER_LIBRARIES uses CMAKE_DL_LIBS, and remove DbgHelp for Windows.

--- share/cframe/modules/CFrameLibraryLoader.cmake
+++ share/cframe/modules/CFrameLibraryLoader.cmake
@@ -11,16 +11,10 @@
 if ( ("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux") OR
      ("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin") )
   set(
-      CFRAME_SYSTEM_LOADER_LIBRARIES dl elf
+      CFRAME_SYSTEM_LOADER_LIBRARIES ${CMAKE_DL_LIBS} elf
       CACHE STRING
       "Libraries to link for runtime dynamic loading."
   )
 else()
-  set(
-      CFRAME_SYSTEM_LOADER_LIBRARIES
-      Dbghelp.lib
-      CACHE STRING
-      "Libraries to link for runtime dynamic loading."
-  )
 
 endif()
