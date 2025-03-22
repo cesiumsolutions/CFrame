966e888f1a6ee5fdb8b45d51432f51b61f6a0ee4
csklu <csklu@cesiumsolutions.com>
2025-03-22 15:53:52 +0100

    CFrame/Extern: Catch2 updated.

--- share/cframe/externals/CFrameSetupCatch2.cmake
+++ share/cframe/externals/CFrameSetupCatch2.cmake
@@ -6,6 +6,32 @@
 
 set( CATCH_ROOT "" CACHE PATH "Path to Catch 2 installation." )
 
+if ( WIN32 )
+
+  if ( "${CATCH_ROOT}" STREQUAL "" )
+
+    cframe_search_paths(
+        Catch2
+        "${CFRAME_EXTERN_SEARCH_PATHS}"
+        CATCH_ROOT
+    )
+
+    if ( "${CATCH_ROOT}" STREQUAL "" )
+      message(
+          FATAL_ERROR
+          "Catch2 Not found, set CATCH_ROOT or CFRAME_EXTERN_SEARCH_PATHS"
+      )
+      return()
+    endif()
+
+  endif()
+
+else()
+
+  find_package( Catch2 )
+
+endif() # Not WIN32
+
 if ( EXISTS "${CATCH_ROOT}" )
 
   list( APPEND CMAKE_MODULE_PATH ${CATCH_ROOT}/lib/cmake/Catch2 )
