68e22b8ebb0b82cb9231703f7230db04d4c96e87
csklu <csklu@cesiumsolutions.com>
2025-03-22 17:36:47 +0100

    CFrame/Modules: C++ configuration updated with new versions.

--- share/cframe/modules/CFrameC++Configuration.cmake
+++ share/cframe/modules/CFrameC++Configuration.cmake
@@ -8,29 +8,26 @@
 # Handle selection of C++ standard Version
 # ----------------------------------------
 set(
-    CFRAME_CXX_STANDARD 14
+    CMAKE_CXX_STANDARD 17
     CACHE STRING "C++ Standard Version to use"
 )
 set_property(
-    CACHE CFRAME_CXX_STANDARD
+    CACHE CMAKE_CXX_STANDARD
     PROPERTY STRINGS
-        97 11 14 17 20
+        98 11 14 17 20 23 26
 )
-if ( NOT ${CFRAME_CXX_STANDARD} EQUAL 97 )
-  set( CMAKE_CXX_STANDARD ${CFRAME_CXX_STANDARD} )
-endif()
 
 if ( WIN32 )
   # Set Compiler version. For Visual Studio, CXX_STANDARD doesn't work, have t0
   # explicitly set it.
   # See: https://developercommunity.visualstudio.com/content/problem/139261/msvc-incorrectly-defines-cplusplus.html
   if ( ${MSVC_VERSION} GREATER 1900  )
-    if ( ${CFRAME_CXX_STANDARD} EQUAL 97 )
+    if ( ${CMAKE_CXX_STANDARD} EQUAL 98 )
       set(
           CFRAME_COMPILE_OPTIONS ${CFRAME_COMPILE_OPTIONS} /Zc:__cplusplus-
           CACHE INTERNAL "Compile Options"
       )
-    elseif ( ${CFRAME_CXX_STANDARD} EQUAL 11 )
+    elseif ( ${CMAKE_CXX_STANDARD} EQUAL 11 )
       set(
           CFRAME_COMPILE_OPTIONS ${CFRAME_COMPILE_OPTIONS} /Zc:__cplusplus
           CACHE INTERNAL "Compile Options"
@@ -46,11 +43,11 @@ if ( WIN32 )
 
 else()
 
-  if ( NOT ${CFRAME_CXX_STANDARD} EQUAL 97 )
+  if ( NOT ${CMAKE_CXX_STANDARD} EQUAL 98 )
     set(
-	    CFRAME_COMPILE_OPTIONS ${CFRAME_COMPILE_OPTIONS}
-		    -std=c++${CFRAME_CXX_STANDARD}
-		CACHE INTERNAL "Compile Options"
+	      CFRAME_COMPILE_OPTIONS ${CFRAME_COMPILE_OPTIONS}
+		    -std=c++${CMAKE_CXX_STANDARD}
+		    CACHE INTERNAL "Compile Options"
 	)
   endif()
 
