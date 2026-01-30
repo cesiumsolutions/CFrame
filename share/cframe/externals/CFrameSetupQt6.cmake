e29717df2a7f6bf8cfaa49044efb2c39d25946c7
csklu <csklu@cesiumsolutions.com>
2026-01-30 09:25:38 +0100

    CFrame/Extern: Qt6 target dependency install macro added.

--- share/cframe/externals/CFrameSetupQt6.cmake
+++ share/cframe/externals/CFrameSetupQt6.cmake
@@ -18,3 +18,21 @@ endif()
 find_package( Qt6 REQUIRED COMPONENTS ${Qt6_COMPONENTS} )
 
 qt_standard_project_setup()
+
+# Installs Qt6 dependencies for specified target
+macro( cframe_qt6_target_install TARGET_NAME )
+
+  if ( WIN32 AND CFRAME_INSTALL_DEPS )
+    qt_finalize_target( ${TARGET_NAME} )
+    qt_generate_deploy_app_script(
+        TARGET ${TARGET_NAME}
+        OUTPUT_SCRIPT ${TARGET_NAME}_QT_DEPLOY_SCRIPT
+        NO_UNSUPPORTED_PLATFORM_ERROR
+        NO_TRANSLATIONS
+    )
+    install(
+        SCRIPT ${${TARGET_NAME}_QT_DEPLOY_SCRIPT}
+    )
+  endif() # CFRAME_INSTALL_DEPS
+
+endmacro() # cframe_qt6_target_install
