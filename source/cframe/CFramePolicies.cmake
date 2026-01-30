9f7ec9414b101206634d278b0f3d3ef7346a58a5
csklu <csklu@cesiumsolutions.com>
2026-01-30 09:24:18 +0100

    CFrame/Source: CMP0087 Policy added.

--- source/cframe/CFramePolicies.cmake
+++ source/cframe/CFramePolicies.cmake
@@ -6,8 +6,20 @@
 # CMP0074: find_package() uses <PackageName>_ROOT variables.
 cmake_policy( SET CMP0074 NEW )
 
+# ``install(SCRIPT)`` did not evaluate generator expressions.  CMake 3.14
+# and later will evaluate generator expressions for ``install(CODE)`` and
+#``install(SCRIPT)``.
+# The ``OLD`` behavior of this policy is for ``install(CODE)`` and
+# ``install(SCRIPT)`` to not evaluate generator expressions.  The ``NEW``
+# behavior is to evaluate generator expressions for ``install(CODE)`` and
+# ``install(SCRIPT)``.
+if ( POLICY CMP0087 )
+  cmake_policy( SET CMP0087 NEW )
+endif()
+
 set( CFRAME_CMAKE_POLICIES
     "CMP0074:NEW" # CMP0074: find_package() uses <PackageName>_ROOT variables.
+    "CMP0087:NEW" # CMP0087: install(SCRIPT) evaluates generator expressions.
     CACHE STRING "List of Policy names and values"
 )
 
